a3e9c6564041ec73b7a32dfa55fd4fe8bb550fb9
Code-Flyer98 <jashkotharitha@gmail.com>
2026-03-03 00:10:32 +0530

    Fix Database URL for database and local

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-db_url="postgresql://postgres:1234@localhost:5432/telusko"
-engine=create_engine(db_url)
+import os
+DATABASE_URL=os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    DATABASE_URL="postgresql://postgres:1234@localhost:5432/telusko"
+engine=create_engine(DATABASE_URL)
 session=sessionmaker(autocommit=False,autoflush=False,bind=engine)
 
